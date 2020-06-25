db27b984ce756ced1fac5e2a61f262f43c02cb40
beer_shigachi <beer.shigach@gmail.com>
2020-06-25 09:17:00 +0900

    Added class TouchScreen.

--- app.py
+++ app.py
@@ -1,11 +1,23 @@
 import kivy
 from kivy.app import App
 from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.widget import Widget
+
+
+class TouchScreen(FloatLayout):
+    def on_touch_down(self, touch):
+        print("touch down", touch)
+
+    def on_touch_up(self, touch):
+        print("touch up")
+
+    def on_touch_move(self, touch):
+        print("touch moving", touch)
 
 
 class MyApp(App):
     def build(self):
-        return FloatLayout()
+        return TouchScreen()
 
 
 if __name__ == "__main__":
