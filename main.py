a2454823bc6b5b3022788653d9d550c1d741edd9
BeerShigachi <beer.shigach@gmail.com>
2020-10-20 15:15:46 +0900

    Optimized screen transition.direction

--- main.py
+++ main.py
@@ -46,7 +46,7 @@ class MyApp(MDApp):
 
     def change_screen(self, screen_name, direction='left'):
         screen_manager = self.root.ids['screen_manager']
-        if screen_name == 'Main' and screen_manager.current == 'Setting':
+        if screen_name == 'History' and screen_manager.current == 'Setting':
             direction = 'right'
         screen_manager.transition.direction = direction
         screen_manager.current = screen_name
