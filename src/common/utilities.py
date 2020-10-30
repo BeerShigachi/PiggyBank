16e74df3519696e03269c5f3c37b5a4a49f80ce8
Beer Shigachi <50823902+BeerShigachi@users.noreply.github.com>
2020-10-30 19:22:16 +0900

    Fixed a bug in valid_user_input() in ./src/common/utilities.py

--- src/common/utilities.py
+++ src/common/utilities.py
@@ -3,13 +3,13 @@ from main import db
 
 def valid_user_input(user_input):
     validation_bool = True
-    if user_input == '' or float(user_input) < 0:
+    if user_input == '-':
+        validation_bool = False
+    elif user_input == '' or float(user_input) < 0:
         validation_bool = False
     elif float(user_input) >= 1 and user_input[0] == '0':
         validation_bool = False
     elif 0 < float(user_input) < 1 and user_input[:2] == '00':
-        validation_bool = False
-    elif user_input == "-":
         validation_bool = False
     return validation_bool
 
